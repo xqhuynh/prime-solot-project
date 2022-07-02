d16403097b220125d58de0b7f6646a0bfd5681d7
Savon Huynh <xqhuynh@gmail.com>
2022-07-01 21:18:59 -0500

    Add navbar/header styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,47 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import { useSelector } from "react-redux";
 
-function Nav() {
+import Nav from "react-bootstrap/Nav";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+
+function Navbar() {
   const user = useSelector((store) => store.user);
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
+    <div>
+      <div className="nav-container">
         {/* If no user is logged in, show these links */}
         {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
+          <Nav defaultActiveKey="/home" as="ul">
+            {/* Clicking on logo will take user /home */}
+            <Link className="home-link" to="/home">
+              <img className="logo" src="./images/logo3.png" alt="logo" />
+            </Link>
+
+            <Nav.Item as="li">
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-1">Shop</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-2">Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-2">About</Nav.Link>
+            </Nav.Item>
+          </Nav>
         )}
 
-        {/* If a user is logged in, show these links */}
+        {/* if user is logged in, show these links */}
         {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
+          <Nav defaultActiveKey="/home" as="ul">
+            {/* Clicking on logo will take user /home */}
+            <Link className="home-link" to="/home">
+              <img className="logo" src="./images/logo3.png" alt="logo" />
             </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
+            <Nav.Item as="li">
+              <Nav.Link href="/home">Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-1">Shop</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-2">Contact</Nav.Link>
+            </Nav.Item>
+            <Nav.Item as="li">
+              <Nav.Link eventKey="link-2">About</Nav.Link>
+            </Nav.Item>
+            {/* Profile and cart icon links */}
+            <div className="profile-cart">
+              <Link to="/login">
+                <AccountCircleRoundedIcon className="profile" />
+              </Link>
+              <ShoppingCartRoundedIcon className="cart" />
+            </div>
+          </Nav>
         )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
       </div>
     </div>
   );
 }
 
-export default Nav;
+export default Navbar;
