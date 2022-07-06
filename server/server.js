a8ef40caa659b90fe88e4388e9ebbf12f0213ebc
Savon Huynh <xqhuynh@gmail.com>
2022-07-06 13:59:11 -0500

    Add cakes route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const cakeRouter = require('./routes/cakes.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/cakes', cakeRouter);
 
 // Serve static files
 app.use(express.static('build'));
