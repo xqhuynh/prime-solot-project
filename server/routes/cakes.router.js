ae67a08cb24adebc090241c47b1bd50dedcdae1e
Savon Huynh <xqhuynh@gmail.com>
2022-07-06 14:06:23 -0500

    Create cakes router

--- server/routes/cakes.router.js
+++ server/routes/cakes.router.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
 router.get('/', (req, res) => {
   // GET route code here
+  console.log('/cake GET route');
+
+  let queryText = `SELECT * FROM product;`;
+
+  pool.query(queryText)
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      console.log('GET /cake error', err);
+      res.sendStatus(500)
+    })
 });
 
 /**
