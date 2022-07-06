74121d13b8aa0f4c556664ece211128faacd0d0d
Savon Huynh <xqhuynh@gmail.com>
2022-07-06 16:27:04 -0500

    Create cakeDetail reducer

---
+++ src/redux/reducers/cakeDetail.reducer.js
@@ -0,0 +1,10 @@
+const activeCake = (state = {}, action) => {
+    switch (action.type) {
+        case 'SET_ACTIVE_CAKE':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default activeCake;
