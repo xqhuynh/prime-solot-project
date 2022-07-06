93022e306cd7e2151c199bd68fac504d71ad4f2f
Savon Huynh <xqhuynh@gmail.com>
2022-07-06 14:33:35 -0500

    Render cakeList to DOM

--- src/components/CakesList/CakesList.jsx
+++ src/components/CakesList/CakesList.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 function CakesList() {
   const dispatch = useDispatch();
+  const cakes = useSelector((store) => store.cakes);
 
   useEffect(() => {
     dispatch({
@@ -15,18 +16,17 @@ function CakesList() {
   return (
     <>
       <h2>Cakes List</h2>
-      {/* <section className="cakes">
-        {cakes.map(cake => {
-            return (
-                <div key={cake.id} >
-                    <Link to={`/cakes/${cake.id}`}>
-                        <h3>{cake.name}</h3>
-                        <img src={cake.img} alt={cake.name} />
-                    </Link>
-                </div>
-            )
+      <section className="cakes">
+        {cakes.map((cake) => {
+          return (
+            <ul key={cake.id}>
+              <li>
+                {cake.name} costs {cake.price}.{cake.description}
+              </li>
+            </ul>
+          );
         })}
-      </section> */}
+      </section>
     </>
   );
 }
