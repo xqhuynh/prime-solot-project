ec7e1bac7c6f8e8b523041a189518db408d52327
Savon Huynh <xqhuynh@gmail.com>
2022-07-12 16:04:09 -0500

    Create tables for admin view and map through items

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,7 +1,96 @@
 import React from "react";
+import "./Admin.css";
+import { useSelector, useDispatch } from "react-redux";
 
 function Admin() {
-  return <div>Admin</div>;
+  // Bring in 'cakes' from redux store
+  const cakes = useSelector((store) => store.cakes);
+
+  return (
+    <>
+      {/* Cake input */}
+      <div className="admin-container">
+        <h2>Admin Page View</h2>
+        <h3>Add Cake</h3>
+        <form>
+          <label>Name</label>
+          <input className="admin-inputs" />
+          <label>Price</label>
+          <input className="admin-inputs" />
+          <label>Upload Photo</label>
+          <input className="admin-inputs" />
+
+          <div>
+            <p>Description</p>
+            <textarea className="admin-inputs" />
+          </div>
+        </form>
+      </div>
+
+      <div className="tables-container">
+        {/* Orders Table */}
+        <div id="admin">
+          <h3>Orders Table</h3>
+          <table className="orders-table">
+            <thead>
+              <tr>
+                <th>Order ID</th>
+                <th>Item</th>
+                <th>Customer Name</th>
+                <th>Total Amount</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cakes.map((item) => (
+                <tr key={item.id}>
+                  <td>Placeholder</td>
+                  <td>Placeholder</td>
+                  <td>Placeholder</td>
+                  <td>Placeholder</td>
+                  <td>Placeholder</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* Inventory Table */}
+          <div id="admin">
+            <h3>Inventory Table</h3>
+
+            <table>
+              <tbody>
+                <tr>
+                  <th>Edit</th>
+                  <th>Item</th>
+                  <th>Price</th>
+                  <th>Description</th>
+                  <th>Delete</th>
+                </tr>
+              </tbody>
+
+              <tbody>
+                {cakes.map((item) => (
+                  <tr key={item.id}>
+                    <td>
+                      <button className="inventory-button">Edit</button>
+                    </td>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>{item.description}</td>
+                    <td>
+                      <button className="inventory-button">Delete</button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      {/* Orders Table */}
+    </>
+  );
 }
 
 export default Admin;
