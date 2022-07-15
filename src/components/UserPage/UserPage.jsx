ce4b5374fdb4fe817c115b304e1d6630974e5d16
Savon Huynh <xqhuynh@gmail.com>
2022-07-15 16:16:52 -0500

    Update favicon and title

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
+import "./UserPage.css";
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   return (
     <div className="container">
-      <h2>Welcome to Sweet Surrender!</h2>
+      <h2 className="welcome-text">Welcome to Sweet Surrender!</h2>
       <LogOutButton className="btn" />
     </div>
   );
