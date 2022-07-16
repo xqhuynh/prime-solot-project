b1007e0d256a93bddecc10a19e532376ce476d6f
Savon Huynh <xqhuynh@gmail.com>
2022-07-16 18:22:02 -0500

    Create dispatch to add cake

--- src/components/AddItemForm/AddItemForm.jsx
+++ src/components/AddItemForm/AddItemForm.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./AddItemForm.css";
 
 function AddItemForm() {
+  const dispatch = useDispatch();
+  // useState to store inputs
+  const [cakeName, setCakeName] = useState("");
+  const [cakePrice, setCakePrice] = useState("");
+  const [cakeDescription, setCakeDescription] = useState("");
+
+  // function to handle submit button, dispatch 'ADD_CAKE'
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    dispatch({
+      type: "ADD_CAKE",
+      payload: {
+        name: cakeName,
+        price: cakePrice,
+        description: cakeDescription,
+      },
+    });
+
+    // Clear input fields after submit
+    setCakeName("");
+    setCakePrice("");
+    setCakeDescription("");
+  };
+
   return (
     <>
       <h2 className="admin-header-title">Admin Page View</h2>
@@ -9,17 +35,30 @@ function AddItemForm() {
       {/* Cake input */}
       <div className="admin-container">
         <h3>Add Cake</h3>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Name</label>
-          <input className="admin-inputs" />
+          <input
+            onChange={(event) => setCakeName(event.target.value)}
+            value={cakeName}
+            className="admin-inputs"
+          />
+
           <label>Price</label>
-          <input className="admin-inputs" />
-          <label>Upload Photo</label>
-          <input className="admin-inputs" />
+          <input
+            onChange={(event) => setCakePrice(event.target.value)}
+            value={cakePrice}
+            className="admin-inputs"
+          />
+          {/* <label>Upload Photo</label>
+          <input className="admin-inputs" /> */}
 
           <div>
             <p>Description</p>
-            <textarea className="admin-inputs" />
+            <textarea
+              onChange={(event) => setCakeDescription(event.target.value)}
+              value={cakeDescription}
+              className="admin-inputs"
+            />
           </div>
           <input type="submit" value="Add Cake" className="admin-inputs" />
         </form>
